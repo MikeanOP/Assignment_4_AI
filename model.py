96e9cece470192b5536ca2b9b13a05f3d9a7c36e
Aryan <agupta28_be24@thapar.edu>
2026-02-18 22:43:25 +0530

    Added linear regression baseline

--- model.py
+++ model.py
@@ -1,3 +1,16 @@
-﻿import pandas as pd
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
-print("starter script")
+data = pd.read_csv("dataset.csv")
+X = data[["size_sqft", "bedrooms", "age_years"]]
+y = data["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+print("R2 score:", r2_score(y_test, pred))
